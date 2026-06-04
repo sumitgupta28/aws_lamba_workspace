3a1d9190614e8a663f4e8f842be6fc2d29afe021
sumit gupta <sumitgupta28@gmail.com>
2026-06-04 00:59:18 -0500

    git updated users

--- terraform-lambda-user-creation/providers.tf
+++ terraform-lambda-user-creation/providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "us-east-1"
   profile = "user-creation"
-  skip_metadata_api_check = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
 }
--- terraform-lambda/main.tf
+++ terraform-lambda/main.tf
@@ -16,6 +16,10 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   profile = "lambda_manager_profile"
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true  
 }
 
 # -----------------------------------------------------------------------
